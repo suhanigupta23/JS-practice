59dd3eadd4d2317307284ee1d1e8a51a3cbca99e
Suhani Gupta <144280247+suhanigupta23@users.noreply.github.com>
2024-10-20 07:55:59 +0000

    functions

---
+++ basics_3/functions.js
@@ -0,0 +1,31 @@
+console.log("E");
+console.log("k");
+console.log("a");
+console.log("s");
+console.log("u");
+//this way is lenghty so we take funtions 
+
+function sayMyName(){
+    console.log("E");
+    console.log("k");
+    console.log("a");
+    console.log("s");
+    console.log("u");
+}
+
+// sayMyName()  //we call function
+
+function addTwoNumber(number1, number2)
+{
+    console.log(number1+number2);
+}
+
+addTwoNumber(3,null)
+
+function addTwoNumber(number1, number2)
+{
+    let result=number1+number2
+    console.log("Ekasu this will be printed")
+    console.log(number1+number2);
+    console.log("The statement after return will not get printed")
+}
